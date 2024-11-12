d49ead6dfd449ce6ace0684d1b3414d9c680b9b2
Linn <32527868+linn-jagestedt@users.noreply.github.com>
2024-11-12 15:15:06 +0100

    Update pc.js

--- InfoVis/js/pc.js
+++ InfoVis/js/pc.js
@@ -1,165 +1,215 @@
-// https://bl.ocks.org/jerdak/5d37e36603bd4397ac51fe5032bcfe3e
-// http://bl.ocks.org/mbostock/3709000
-// http://plnkr.co/edit/DquAXNv2mbbok7ssNuoX?p=preview&preview
+// https://www.d3-graph-gallery.com/graph/interactivity_brush.html
 
-// Static PC
-function pc(data) {
+function labcode(data, x_var, y_var, sp_svg, tooltip) {
 
-	// Set width and height of the chart
-	var div = '#pc';
-	var margin = { top: 30, right: 10, bottom: 30, left: 320 },
-		width = $('div.pc-container').width(),
-		height = $('div.pc-container').height();
+  //------------------------------------------------------------------------------------->
+  /** COMPUTER EXERCISE STARTS HERE  */
+  //Task 5.1.1  -- Create the x-axis
+  var x = d3.scaleLinear()
+    .domain([d3.min(data), d3.max(data)])
+    .range([0, width / 2.0]);
 
-	var x = d3.scaleBand().range([0, width]);
-	var yAxis = d3.axisLeft();
-	var y = {};
+    
 
-	var line = d3.line(),
-		foreground,
-		dimensions;
+  //Task 5.1.2  -- Append the axes to the svg
+  var xAxis = sp_svg.append("g")
+  .attr("transform", "translate(" + 0 + ", " + 400 + ")")
+  .call(d3.axisBottom(x));
 
-	var extents, origDimensions, dragging = {};
+  //Task 5.1.3  -- Create y-axis
+  var y = d3.scaleLinear()
+  .domain([d3.min(data), d3.max(data)])
+  .range([400, 0]);
 
-	var pc_svg = d3.selectAll(div)
-		.attr('viewBox', '0 0 ' + (
-			width + margin.left + margin.right)
-			+ ' ' + (height + margin.top + margin.bottom))
-		.attr('height', height)
-		.attr('width', '100%')
-		.attr('preserveAspectRatio', 'none')
-		.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+  // Task 5.1.4 -- Append the axis to svg
+  var yAxis = sp_svg.append("g")
+  .attr("transform", "translate(" + 0 + ", " + 0 + ")")
+  .call(d3.axisLeft(y));
 
-	x.domain(dimensions = d3.keys(data[0]).filter(function (d) {
-		return d != "Region" && (y[d] = d3.scaleLinear()
-			.domain(d3.extent(data, function (p) { return +p[d]; }))
-			.range([height, 0]));
-	}));
+  // Task 5.1.5 -- Append circles to svg
+  var myCircles = sp_svg.append("g")
+  .selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr("r", 6)
+  .attr("fill", "darkturquoise")
+  .attr("opacity", 0.3);
 
-	//------------------------------------------------------------------------------------->
-	/** COMPUTER EXERCISE STARTS HERE  */
-	origDimensions = dimensions.slice(0);
-	extents = dimensions.map(function (p) { return [0, 0]; });
-
-	// Task 5.2.1 -- Drawing the Lines
-	var foreground;
-
-	
-	// Task 5.2.2 -- Drawing Axes
-	var axes;
-
-	
-	// 5.2.3 -- Appending Axes Titles
+  hovering(myCircles, tooltip);
 
 
-	// 5.2.4 -- Interaction, brushing the axes
-	
+  // Task 5.1.7 -- Adding hovering
 
-	// 5.2.5 -- Interaction, dragging the Axes
-	
 
-	//------------------------------------------------------------------------------------->
-	/** COMPUTER EXERCISE ENDS HERE  **/
-
-	/** NECESSARY FUNCTION. DO NOT TOUCH */
-	function perAxisBrush(d) {
-		return y[d].brush = d3.brushY()
-			.extent([[-10, 0], [10, height]])
-			.on("brush", brush)
-			.on("end", brush)
-	}
-
-	function startDrag(d) {
-		dragging[d] = x(d);
-	}
-
-	function drag(d) {
-		dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-		foreground.attr("d", drawPath);
-		dimensions.sort(function (a, b) { return position(a) - position(b); });
-		x.domain(dimensions);
-		axes.attr("transform", function (d) { return "translate(" + position(d) + ")"; })
-	}
-
-	function endDrag(d) {
-		delete dragging[d];
-		transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-		transition(foreground).attr("d", drawPath);
-
-		var new_extents = [];
-		for (var i = 0; i < dimensions.length; ++i) {
-			new_extents.push(extents[origDimensions.indexOf(dimensions[i])]);
-		}
-		extents = new_extents;
-		origDimensions = dimensions.slice(0);
-	}
-
-	//------------------------------------------------------------------------------------->
-	// Returns the path for a given data point
-	function path(d) {
-		return line(dimensions.map(function (p) { return [x(p), y[p](d[p])]; }));
-	} // End of path
-
-	//------------------------------------------------------------------------------------->
-	// Handles a brush event, toggling the display of foreground lines
-	function brush() {
-		var actives = [];
-		pc_svg.selectAll(".brush")
-			.filter(function (d) {
-				y[d].brushSelectionValue = d3.brushSelection(this);
-				return d3.brushSelection(this);
-			})
-			.each(function (d) {
-				actives.push({
-					dimension: d,
-					extent: d3.brushSelection(this).map(y[d].invert)
-				});
-			});
-
-		// Update foreground to only display selected values
-		foreground.style("display", function (d) {
-			return actives.every(function (active) {
-				return active.extent[1] <= d[active.dimension] && d[active.dimension] <= active.extent[0];
-			}) ? null : "none";
-		});
-
-	}// End of brush
-
-	// Returns the path for a given data point
-	function drawPath(d) {
-		return line(dimensions.map(function (p) {
-			return [x(p), y[p](d[p])];
-		}));
-	}
-
-	this.selectLine = function (value) {
-		d3.select(".foreground").selectAll('path')
-			.style("stroke", function (d) {
-				return d.Region == value ? "deeppink" : 'darkturquoise';
-			})
-			.style("stroke-width", function (d) {
-				return d.Region == value ? "3.0" : '0.3';
-			})
-			.style("opacity", function (d) {
-				return d.Region == value ? "1.0" : '0.3';
-			})
-	}
-
-	this.resetSelectLine = function () {
-		var l = d3.select(".foreground");
-		l.selectAll("path")
-			.style("stroke", "darkturquoise")
-			.style("stroke-width", '0.4px')
-			.style("opacity", '1.0')
-	}
-
-	function position(d) {
-		var v = dragging[d];
-		return v == null ? x(d) : v;
-	}
-
-	function transition(axes) {
-		return axes.transition().duration(300);
-	}
+  return [x, xAxis, y, yAxis, myCircles];
 }
+//------------------------------------------------------------------------------------->
+/** COMPUTER EXERCISE ENDS HERE  **/
+
+/** NECESSARY FUNCTION. DO NOT TOUCH */
+function sp(data) {
+
+  const features = data.columns;
+  // Take the second column as default (exclude first column)
+  const defaultX = features[1].toString();
+  const defaultY = features[1].toString();
+
+  var x_var = defaultX;
+  var y_var = defaultY;
+
+  var vars = []
+
+  // Skipping the first feature
+  for (i = 1; i < features.length; i++) {
+    vars.push(features[i])
+  }
+
+  var dropDownX = d3.select("#dropdownX")
+    .selectAll("myOptions")
+    .data(vars)
+    .enter()
+    .append('option')
+    .property("selected", function (d) {
+      return d === defaultX;
+    })
+    .text(function (d) { return d; }) // Text showed in the menu
+    .attr("value", function (d) { return d; }) // Corresponding value returned by the button
+
+  var dropDownY = d3.select("#dropdownY")
+    .selectAll("myOptions")
+    .data(vars)
+    .enter()
+    .append('option')
+    .property("selected", function (d) {
+      return d === defaultY;
+    })
+    .text(function (d) { return d; }) // Text showed in the menu
+    .attr("value", function (d) { return d; }) // Corresponding value returned by the button
+
+  var margin = { top: 10, right: 40, bottom: 10, left: 60 },
+    width = $('div.sp-container').width(),
+    height = $('div.sp-container').height();
+
+  var sp_svg = d3.select("#sp")
+    .append("svg")
+    .attr('viewBox', '0 0 ' + (
+      width + margin.left + margin.right)
+      + ' ' + (height + margin.top + margin.bottom))
+    .attr('width', '100%')
+    .attr('height', height)
+    .attr("preserveAspectRatio", "xMidYMid meet")
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  // Tooltip
+  var tooltip = d3.select("#sp").append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+  /**
+   * Scatter Plot (here is where your code is used for the scatter plot)
+   */
+  var values = labcode(data, x_var, y_var, sp_svg, tooltip);
+
+  var x = values[0];
+  var xAxis = values[1];
+  var y = values[2];
+  var yAxis = values[3];
+  var myCircles = values[4];
+
+  this.selectDot = function (value) {
+    //Call focusCircle
+    focusCircle(value)
+  }
+
+  this.resetSelectDot = function () {
+    d3.selectAll("circle")
+      .style("fill", "darkturquoise")
+      .style("opacity", '0.3')
+      .attr('r', 6)
+  }
+
+  // A function that update the chart
+  function update(selectedX, selectedY) {
+    x.domain([
+      d3.min(data, function (d) { return +d[selectedX]; }),
+      d3.max(data, function (d) { return +d[selectedX]; })
+    ])
+    xAxis
+      .transition()
+      .duration(400)
+      .call(d3.axisBottom(x))
+
+    y.domain([
+      d3.min(data, function (d) { return +d[selectedY] }),
+      d3.max(data, function (d) { return +d[selectedY] })
+    ])
+    yAxis
+      .transition()
+      .duration(400)
+      .call(d3.axisLeft(y))
+
+    // Give these new data to update 
+    myCircles
+      .data(data)
+      .transition()
+      .duration(400)
+      .attr("cx", function (d) { return x(+d[selectedX]); })
+      .attr("cy", function (d) { return y(+d[selectedY]); })
+  }
+
+  d3.selectAll("#dropdownX, #dropdownY").on("change", function (d) {
+
+    // Recover the option that has been chosen
+    var selectedOptionX = d3.select("#dropdownX").property("value")
+    var selectedOptionY = d3.select("#dropdownY").property("value")
+    update(selectedOptionX, selectedOptionY)
+  })
+}
+
+function hovering(myCircles, tooltip) {
+  myCircles.on("mouseover", function (d) {
+    focusCircle(d.Region)
+    pc.selectLine(d.Region)
+    tooltip
+      .style("opacity", 1)
+      .html("Region: " + d.Region)
+      .style("left", (d3.mouse(this)[0] + 25) + "px") // It is important to put the +25: else the tooltip is exactly where the mouse is 
+      .style("top", (d3.mouse(this)[1] - 25) + "px")
+
+  })
+    .on("mouseleave", function (d) {
+      pc.resetSelectLine()
+      d3.selectAll("circle")
+        .style("fill", "darkturquoise")
+        .style("opacity", '0.3')
+        .attr('r', 6);
+      tooltip
+        .transition()
+        .duration(50)
+        .style("opacity", 0)
+    })
+}
+
+function focusCircle(circle) {
+  var d = d3.selectAll("circle");
+  d.style("fill", function (d) {
+    return d.Region == circle ? "deeppink" : 'darkturquoise';
+  })
+    .style("opacity", function (d) {
+      return d.Region == circle ? "1.0" : '0.3';
+    })
+    .attr("r", function (d) {
+      return d.Region == circle ? 10 : 6;
+    })
+    .style("stroke-width", function (d) {
+      return d.Region == circle ? 1 : 0;
+    })
+}
+/**END OF SP*/
