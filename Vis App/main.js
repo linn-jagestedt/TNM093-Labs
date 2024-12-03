57191a5a68ac3c80505a2a2127d23425bd7cd08c
Linn <32527868+linn-jagestedt@users.noreply.github.com>
2024-12-03 16:11:19 +0100

    Almost done

--- Vis App/main.js
+++ Vis App/main.js
@@ -23,11 +23,16 @@ let cols = parseInt(document.getElementById("cols").value, 10);
 let restoreForce = parseFloat(document.getElementById("restore-force").value);
 let damping = parseFloat(document.getElementById("damping").value);
 const nodeRadius = 15;
-const timeStep = 0.016;
+const timeStep = 0.16;
 const padding = 50;
+const mass = 2;
+
+let xStep = (width - 2 * padding) / (cols - 1);
+let yStep = (height - 2 * padding) / (rows - 1);
 
 // Arrays to hold positions, velocities, and forces
 let positions = [];
+let lastPositions = [];
 let velocities = [];
 let forces = [];
 let isRunning = false;
@@ -39,19 +44,20 @@ function initializeGrid() {
     positions = [];
     velocities = [];
     forces = [];
-    const xStep = (width - 2 * padding) / (cols - 1);
-    const yStep = (height - 2 * padding) / (rows - 1);
+    xStep = (width - 2 * padding) / (cols - 1);
+    yStep = (height - 2 * padding) / (rows - 1);
 
     for (let i = 0; i < rows; i++) {
         const positionRow = [];
         const velocityRow = [];
         const forceRow = [];
         for (let j = 0; j < cols; j++) {
-            positionRow.push([padding + xStep * j, padding + yStep * i]); // ! TODO: think about how to calculate initial positions for the nodes
+            positionRow.push([padding + xStep * j, padding + yStep * i]); 
             velocityRow.push([0, 0]); // Initial velocity
             forceRow.push([0, 0]); // Initial force
         }
         positions.push(positionRow);
+        lastPositions.push(positionRow);
         velocities.push(velocityRow);
         forces.push(forceRow);
     }
@@ -85,9 +91,7 @@ function drawNodes() {
  */
 function drawEdges() 
 {
-    let linkGen = d3.linkHorizontal(); // Start horizontal link
-
-    let linePositions = [];
+    const linesData = [];
 
     //
     // Add all the points as links to linePositions
@@ -97,31 +101,53 @@ function drawEdges()
     {
         for (let j = 0; j < cols; j++) 
         {
+            // Right
             if (i < rows - 1) {
-                linePositions.push({
-                    source: positions[i + 0][j],
-                    target: positions[i + 1][j]
-                });
+                linesData.push([
+                    positions[i + 0][j],
+                    positions[i + 1][j]
+                ]);
             }
 
+            // Upp
             if (j < cols - 1) {
-                linePositions.push({
-                    source: positions[i][j + 0],
-                    target: positions[i][j + 1]
-                });
+                linesData.push([
+                    positions[i][j + 0],
+                    positions[i][j + 1]
+                ]);
+            }
+
+            // Left Down
+            if (j > 0 && i < rows - 1) {
+                linesData.push([
+                    positions[i + 0][j],
+                    positions[i + 1][j - 1]
+                ]);
+            }
+
+            //Right down
+            if (j < cols - 1 && i < rows - 1) {
+                linesData.push([
+                    positions[i + 0][j],
+                    positions[i + 1][j + 1]
+                ]);
             }
         }
     }
+
+    const lineGenerator = d3.line();
 
     // 
     // Add links ass paths to svg
     //
 
-    svg
-        .selectAll("path")
-        .data(linePositions)
-        .join("path")
-        .attr("d", linkGen)
+    const lines = svg.selectAll("path").data(linesData);
+    
+    lines
+        .enter()
+        .append("path")
+        .merge(lines)
+        .attr("d", lineGenerator)
         .attr("fill", "none")
         .attr("stroke", "black")
         .attr("thickness", 10);
@@ -133,39 +159,144 @@ function drawEdges()
  */
 function calculateForces() {
     // Reset forces
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++) 
+        {
             forces[i][j][0] = 0;
             forces[i][j][1] = 0;
         }
     }
 
-    // TODO: add your implementation here.
-    // Example:
-    // - Calculate spring forces (horizontal, vertical, diagonal/sheer).
-    // - Add restoring forces.
-    // - Add damping forces.
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++) 
+        {
+            if (i == selectedRow && j == selectedCol) {
+                continue;
+            }
+
+            let springForce = sumForces(i, j);
+
+            forces[i][j][0] += springForce[0];
+            forces[i][j][1] += springForce[1];
+        }
+    }
 }
 
-function updatePositions() {
-    // TODO: think about how to calculate positions and velocities. (e.g. Euler's method)
-    //   calculateForces();
+function sumForces(row, col) 
+{
+    let sum = [0, 0];
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            // TODO: potentially implement position and velocity updates here.
-            // Example:
-            // velocities[i][j][0] += some calculation
-            // velocities[i][j][1] += some calculation
-            // positions[i][j][0] += some calculation;
-            // positions[i][j][1] += some calculation;
+    for (let i = row - 1; i < row + 2; i++) 
+    {
+        for (let j = col - 1; j < col + 2; j++) 
+        {
+            if (i < 0 || j < 0 || i > rows - 1 || j > cols - 1) {
+                continue;
+            }
+
+            if (i == row && j == col) {
+                continue;
+            }
+
+            let len = 0;
+
+            if (i == row) {
+                len = [xStep, 0];
+            } else if (j == col) {
+                len = [0, yStep];
+            } else {
+                //len = Math.sqrt(xStep * xStep + yStep * yStep);
+                len = [xStep, yStep];
+            }
+
+            let springForce = calculateSpringForce(
+                positions[row][col], 
+                positions[i][j], 
+                len
+            );
+    
+            let damping = calculateDampingForce(
+                velocities[row][col],
+                velocities[i][j],
+                len
+            );
+    
+            sum[0] += springForce[0] + damping[0];
+            sum[1] += springForce[1] + damping[1];
+        }
+    }  
+
+    return sum;
+}
+
+function calculateSpringForce(p1, p2, len) 
+{
+    let diffx = p1[0] - p2[0];
+    let diffy = p1[1] - p2[1];
+
+    force = [0, 0];
+
+    if (diffx != 0) {
+        force[0] = -restoreForce * (Math.abs(diffx) - len[0]) * (diffx / Math.abs(diffx));
+    }
+
+    if (diffy != 0) {
+        force[1] = -restoreForce * (Math.abs(diffy) - len[1]) * (diffy / Math.abs(diffy));
+    }
+
+    return force;
+} 
+
+function calculateDampingForce(v1, v2) 
+{
+    let diffx = v1[0] - v2[0];
+    let diffy = v1[1] - v2[1];
+
+    return [
+        -damping * (diffx),
+        -damping * (diffy)
+    ]
+} 
+
+function updatePositionsEuler() 
+{
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++) 
+        {
+            velocities[i][j][0] += timeStep * (forces[i][j][0] / mass);
+            velocities[i][j][1] += timeStep * (forces[i][j][1] / mass);
+
+            positions[i][j][0] += timeStep * velocities[i][j][0];
+            positions[i][j][1] += timeStep * velocities[i][j][1];
         }
     }
 
-    // TODO: Think about how to redraw nodes and edges with updated positions
-    //   drawNodes();
-    //   drawEdges();
+    drawEdges();
+    drawNodes();
 }
+
+function updatePositionsVerlet() 
+{
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++) 
+        {
+            let lastPos = positions[i][j]; // n
+
+            positions[i][j][0] = 2 * positions[i][j][0] - lastPositions[i][j][0] + (forces[i][j][0] / mass) * timeStep * timeStep; // n + 1
+            positions[i][j][1] = 2 * positions[i][j][1] - lastPositions[i][j][1] + (forces[i][j][1] / mass) * timeStep * timeStep; // n + 1
+        
+            lastPositions[i][j] = lastPos; // n - 1
+        }
+    }
+
+    drawEdges();
+    drawNodes();
+}
+
 
 /**
  * Main simulation loop.
@@ -174,8 +305,8 @@ function updatePositions() {
 function simulationLoop() {
     if (!isRunning) return;
 
-    // TODO: think about how to implement the simulation loop. below are some functions that you might find useful.
-    updatePositions();
+    calculateForces() 
+    updatePositionsVerlet();
     requestAnimationFrame(simulationLoop);
 }
 
@@ -212,6 +343,62 @@ document.getElementById("damping").addEventListener("input", (e) => {
     damping = parseFloat(e.target.value);
     document.getElementById("damping-value").textContent = damping.toFixed(2);
 });
+
+let selectedRow = -1;
+let selectedCol = -1;
+
+const simulationArea = document.getElementById("simulation-area");
+
+function getMousePos(e) {
+    var rect = simulationArea.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+
+// Mouse event
+simulationArea.addEventListener("mousedown", (e) => 
+{
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++) 
+        {
+            let mouse = getMousePos(e);
+
+            let diffx = Math.abs(mouse.x - positions[i][j][0]);
+            let diffy = Math.abs(mouse.y - positions[i][j][1]);
+
+            if (Math.sqrt(diffx * diffx + diffy * diffy) < nodeRadius) {
+                selectedRow = i;
+                selectedCol = j;
+            }
+        }
+    }
+});
+
+simulationArea.addEventListener("mouseup", (e) => 
+{
+    selectedRow = -1;
+    selectedCol = -1;
+});
+
+document.getElementById("simulation-area").addEventListener("mousemove", (e) => 
+{
+    if (selectedRow < 0 || selectedCol < 0) {
+        return;
+    }
+
+    let mouse = getMousePos(e);
+
+    positions[selectedRow][selectedCol][0] = mouse.x;
+    positions[selectedRow][selectedCol][1] = mouse.y;
+    
+    drawEdges();
+    drawNodes();
+});
+    
+
 
 // Initialize the simulation
 initializeGrid();
